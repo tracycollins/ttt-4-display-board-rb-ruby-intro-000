e3d76187f305f56e9bfaecb30c512ae4b422abd6
Tracy Collins <tc@threeceemedia.com>
2020-02-13 17:36:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 def display_board(boardValues)
   puts " #{boardValues[0]} | #{boardValues[1]} | #{boardValues[2]} "
   puts "-----------"
-  puts " #{boardValues[0]} | #{boardValues[1]} | #{boardValues[2]} "
+  puts " #{boardValues[3]} | #{boardValues[4]} | #{boardValues[5]} "
   puts "-----------"
+  puts " #{boardValues[6]} | #{boardValues[7]} | #{boardValues[8]} "
 end
